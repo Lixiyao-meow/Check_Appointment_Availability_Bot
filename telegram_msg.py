1e25ac2d891bfd2f36488381e0e47e8e154ff20c
lixiyao_meow <lixiyao0218@hotmail.com>
2023-09-18 16:38:44 +0100

    change config file name

--- telegram_msg.py
+++ telegram_msg.py
@@ -3,12 +3,12 @@ import json
 
 def send_telegram_msg(message):
 
-    with open("telegram_config.json", "r") as f:
+    with open("config.json", "r") as f:
         config = json.load(f)
     
     TOKEN = config["TOKEN"]
     chat_id = config["chat_id"]
-    
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 
     # send message
